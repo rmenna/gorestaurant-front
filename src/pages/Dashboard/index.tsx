c1c86be6493809c47ad035aaeb680f76c5922aa1
Rodrigo Menna <rmenna.barroso@gmail.com>
2020-06-22 21:52:25 -0400

    finalized

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,10 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      const response = await api.get('/foods');
+
+      setFoods(response.data)
+
     }
 
     loadFoods();
@@ -38,6 +41,17 @@ const Dashboard: React.FC = () => {
   ): Promise<void> {
     try {
       // TODO ADD A NEW FOOD PLATE TO THE API
+      const newFood = {
+        id: foods[foods.length - 1] ? foods[foods.length - 1].id + 1 : 1,
+        name: food.name,
+        description: food.description,
+        price: food.price,
+        available: true,
+        image: food.image
+      }
+
+      await api.post('/foods', newFood)
+      setFoods([...foods, newFood])
     } catch (err) {
       console.log(err);
     }
@@ -47,10 +61,31 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     // TODO UPDATE A FOOD PLATE ON THE API
+    const newFoodList = foods.map(food => {
+      if (food.id !== editingFood.id) {
+        return food;
+      }
+
+      return {
+        ...food,
+        id: editingFood.id,
+        available: editingFood.available
+      }
+    })
+
+    setFoods(newFoodList)
+    await api.put(`/foods/${editingFood.id}`, {
+      ...food,
+      id: editingFood.id,
+      available: editingFood.available
+    })
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
     // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+    const newFoodList = foods.filter(food => food.id !== id);
+    setFoods(newFoodList)
   }
 
   function toggleModal(): void {
@@ -63,6 +98,8 @@ const Dashboard: React.FC = () => {
 
   function handleEditFood(food: IFoodPlate): void {
     // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food)
+    toggleEditModal()
   }
 
   return (
